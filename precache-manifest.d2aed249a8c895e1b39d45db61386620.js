46042cbd327a83b25b7527f4064769d677a0b043
Taylor Lee <taylor.dennis.lee@gmail.com>
2020-10-18 13:48:48 -0500

    React

---
+++ precache-manifest.d2aed249a8c895e1b39d45db61386620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39596a1044f479c831356f2bdac855a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9b55e4440ef85e1f97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b018b5e3f24700f837c",
+    "url": "/static/js/2.b3c8dc91.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b3c8dc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9b55e4440ef85e1f97",
+    "url": "/static/js/main.99d0d932.chunk.js"
+  },
+  {
+    "revision": "8e16a0d7a09a839ddc6c",
+    "url": "/static/js/runtime-main.c4c8c8e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
